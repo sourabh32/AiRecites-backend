5a59cc9a01f6b0f2f83b204b901532427c9f2c69
sourabh@32 <sourabhsharma181003@gmail.com>
2023-10-01 22:11:55 +0530

    smae

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,9 @@ const port  = process.env.PORT || 4000
 
 connectDb()
 const app = express()
-app.use(cookieParser())
 app.use(cors())
+app.use(cookieParser())
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

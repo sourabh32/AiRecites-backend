14a48e6656e74318210696a58ee1c1fbc41204fb
sourabh@32 <sourabhsharma181003@gmail.com>
2023-10-01 17:56:29 +0530

    setting cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,12 @@ const port  = process.env.PORT || 4000
 
 connectDb()
 const app = express()
+app.use(cookieParser())
 app.use(cors({
     origin: 'http://localhost:5173', 
     credentials: true, 
   }))
-app.use(cookieParser())
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

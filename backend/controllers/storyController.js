6f0d7e50340072268b29e3881c3b6348722e7d0c
sourabh@32 <sourabhsharma181003@gmail.com>
2023-10-02 11:13:40 +0530

    modifiying storyController

--- backend/controllers/storyController.js
+++ backend/controllers/storyController.js
@@ -7,13 +7,7 @@ const generateStoryController = asyncHandler(async (req,res)=>{
         try {
            const response = await generateStory(prompt,genre);
           let story = response[0].generated_text.replace(/\n/g,'')
-            data = await Story.create({
-            prompt,
-            story,
-            email:req.user.email,
-            name:req.user.name,
-            genre
-           })
+            data = {story}
            res.status(201).json(data)
         } catch (error) {
             res.status(400).json({ error: error.message });
